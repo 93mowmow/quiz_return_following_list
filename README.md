9cb24d2b160f615870063af8726cb505ff9ee4fe
yimaohsiao <yimaohsiao@gmail.com>
2016-12-17 15:38:33 +0800

    Add implementation notes & comment of source code.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # quiz_return_following_list
 Given word = "word" , return following list(order does not matter): ["word","1ord","w1rd","wo1d","wor1","2rd","w2d","wo2","1o1d","1or1","w1r1","1o2","2r1","3d","w3","4"]
+
+# Implementation notes:
+
+1. I observe that the digit next to a number, it could be NULL or "word" char.
+   The digit next to a "word" char, it would only be number or "word" char, can not be a NULL.
+   So, I design flag and word_flag for this rule.
+   
+   a=rand()%random_number_size+1;
+   
+   This will skip the number "0" case, so that a NULL will be skipped.
+   
+2. Inorder to balance the probility of "word" char and number(1,2,3,4), I chose nine for the random number size.
+
+3. Testing:
+
+   If the first digit is "word" char, it is always "w".
+   If the last digit is "word" char, it is always "d".
+   No continue digits of number(1,2,3,4), such as w13, 1234....
--- word.c
+++ word.c
@@ -1,45 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+/*
+Inorder to balance the probility of "word" char and numner(1,2,3,4), I chose nine for the random number size.
+*/
 #define random_number_size 9
 
 int main(void){
 
 	int i = 0;
 	int a = 0;
-	int k = 100;
+	int k = 100; // numner of samples
 	char word[10] = "word";
 	char ch;
-	int flag = 0;
-	int word_flag = 0;
-	srand(time(NULL)); 
+	int flag = 0; // flag for last digit is number(1,2,3,4)
+	int word_flag = 0; // flas for last digit is "word" char
+	srand(time(NULL)); // Use time as the random seed
 
-	while(k>0){
+	while( k > 0 ){
 
 		for(i=0;i<4;i++){
 			
 			if (word_flag == 0)
 				a=rand()%random_number_size;
 			else
-				a=rand()%random_number_size+1;
+				a=rand()%random_number_size+1; //This will skip the number "0" case, so that a NULL will be skipped.
 				
-			if(a>4){
+			if( a > 4 ){ // 5,6,7,8,9
 				
 				flag = 0;
 				word_flag = 1;
-				printf("%c",word[i]);
+				printf("%c",word[i]); // base on the string "word" digits
 			
-			}else{
-			
-				if(a==0){
+			} else { 
 				
-					word_flag = 0;
-					printf("");
+				word_flag = 0;
 				
-				}else{	
+				if( a == 0 ){	
+					
+					printf(""); // a NULL case
 				
-					word_flag = 0;
+				} else { // 1,2,3,4
 				
 					if (flag == 1)
 						continue;
